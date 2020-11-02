1edca50feb12bf95b77a4d6cd2bf7140be2e0f0a
Luis Febro <mr.febro@gmail.com>
2020-11-02 16:43:33 -0400

    frontend 1

--- pages/demo/posts/[id].js
+++ pages/demo/posts/[id].js
@@ -1,46 +1,46 @@
-import Layout from "../../../components/_Layout";
-import { getAllPostIds, getPostData } from "../../../demo/lib/posts";
-import Head from "next/head";
-import Date from "../../../components/demo/date";
-import utilStyles from "../../../styles/utils.module.css";
+// import Layout from "../../../components/_Layout";
+// import { getAllPostIds, getPostData } from "../../../demo/lib/posts";
+// import Head from "next/head";
+// import Date from "../../../components/demo/date";
+// import utilStyles from "../../../styles/utils.module.css";
 
-export default function Post({ postData }) {
-    return (
-        <Layout>
-            <Head>
-                <title>{postData.title}</title>
-            </Head>
-            <article>
-                <h1 className={utilStyles.headingXl}>{postData.title}</h1>
-                <div className={utilStyles.lightText}>
-                    <Date dateString={postData.date} />
-                </div>
-                <div
-                    dangerouslySetInnerHTML={{ __html: postData.contentHtml }}
-                />
-            </article>
-        </Layout>
-    );
-}
+// export default function Post({ postData }) {
+//     return (
+//         <Layout>
+//             <Head>
+//                 <title>{postData.title}</title>
+//             </Head>
+//             <article>
+//                 <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+//                 <div className={utilStyles.lightText}>
+//                     <Date dateString={postData.date} />
+//                 </div>
+//                 <div
+//                     dangerouslySetInnerHTML={{ __html: postData.contentHtml }}
+//                 />
+//             </article>
+//         </Layout>
+//     );
+// }
 
-// In production, getStaticPaths runs at build time.
-// n3. catch-all routes.
-export async function getStaticPaths() {
-    const paths = getAllPostIds();
-    return {
-        paths, // n1
-        fallback: false, // n2
-    };
-}
+// // In production, getStaticPaths runs at build time.
+// // n3. catch-all routes.
+// export async function getStaticPaths() {
+//     const paths = getAllPostIds();
+//     return {
+//         paths, // n1
+//         fallback: false, // n2
+//     };
+// }
 
-export async function getStaticProps({ params }) {
-    const postData = await getPostData(params.id);
-    return {
-        props: {
-            postData,
-        },
-    };
-}
+// export async function getStaticProps({ params }) {
+//     const postData = await getPostData(params.id);
+//     return {
+//         props: {
+//             postData,
+//         },
+//     };
+// }
 
 /* COMMENTS
 n1:
