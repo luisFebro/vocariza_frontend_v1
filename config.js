703f56377b4997c1320f3066914fac46da04cb4d
Luis Febro <mr.febro@gmail.com>
2020-11-03 09:38:10 -0400

    frontend tweaks - env variables

--- config.js
+++ config.js
@@ -1,14 +1,20 @@
 import getConfig from "next/config";
 const { publicRuntimeConfig } = getConfig();
 
-export const API = publicRuntimeConfig.PRODUCTION
-    ? publicRuntimeConfig.API_PRODUCTION
-    : publicRuntimeConfig.API_DEVELOPMENT;
+const {
+    APP_NAME: APP_N,
+    PRODUCTION,
+    API_PRODUCTION,
+    API_DEVELOPMENT,
+    DOMAIN_PRODUCTION,
+    DOMAIN_DEVELOPMENT,
+    FB_APP_ID: FB_ID,
+} = publicRuntimeConfig;
 
-export const APP_NAME = publicRuntimeConfig.APP_NAME;
+export const API = PRODUCTION ? API_PRODUCTION : API_DEVELOPMENT;
 
-export const DOMAIN = publicRuntimeConfig.PRODUCTION
-    ? publicRuntimeConfig.DOMAIN_PRODUCTION
-    : publicRuntimeConfig.DOMAIN_DEVELOPMENT;
+export const APP_NAME = APP_N;
 
-export const FB_APP_ID = publicRuntimeConfig.FB_APP_ID;
+export const DOMAIN = PRODUCTION ? DOMAIN_PRODUCTION : DOMAIN_DEVELOPMENT;
+
+export const FB_APP_ID = FB_ID;
