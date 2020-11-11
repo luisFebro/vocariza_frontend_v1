054e31e29f467336c37a15b8162769f279967d1b
Luis Febro <mr.febro@gmail.com>
2020-11-10 21:54:48 -0400

    blog - vocariza pt 2.3

--- config.js
+++ config.js
@@ -1,20 +1,21 @@
 // warning:
 // next/config is a stateful module, it's only initialized once Next.js is initialized. It's generally not recommended to use it anywhere other than components/pages, because it depends on Next being initialized. Alternatively you can require('./next.config.js') and read the values that way.
-const { publicRuntimeConfig = {} } = require("./next.config.js");
+const { publicRuntimeConfig } = require("./next.config.js");
+console.log("publicRuntimeConfig", publicRuntimeConfig);
 
 const {
     APP_NAME: APP_N,
     FB_APP_ID: FB_ID,
+    IS_PROD,
+    API_PRODUCTION,
+    API_DEVELOPMENT,
     DOMAIN_PRODUCTION,
     DOMAIN_DEVELOPMENT,
 } = publicRuntimeConfig;
 
-const IS_PROD = process.env.NODE_ENV === "production";
+// const IS_PROD = process.env.NODE_ENV === "production";
 
-const API = IS_PROD
-    ? "https://vocariza.herokuapp.com/api"
-    : "http://localhost:5002/api";
-
+const API = IS_PROD ? API_PRODUCTION : API_DEVELOPMENT;
 const DOMAIN = IS_PROD ? DOMAIN_PRODUCTION : DOMAIN_DEVELOPMENT;
 const DOMAIN_PROD = DOMAIN_PRODUCTION;
 
